cc88c22985bcc64d6bca64d42e4abf55e85f192b
Bhanavi Senthil <senthilbhanavi@gmail.com>
2026-02-11 21:31:08 -0500

    finished rest of homework
    homweork hoemwork bad

--- Math2001/01_Proofs_by_Calculation/hw0.lean
+++ Math2001/01_Proofs_by_Calculation/hw0.lean
@@ -13,4 +13,20 @@ for clearer statements and any special instructions. -/
 
 @[autograded 5]
 theorem problem1 {n : ℤ} (hn : n ≥ 5) : n ^ 2 > 2 * n + 11 :=
-  sorry
+  by
+  -- write n = 5 + k with k ≥ 0 and expand
+  let k := n - 5
+  have hk : 0 ≤ k := by rel [hn]
+  have : n = 5 + k := by ring
+  calc
+    n ^ 2 - 2 * n - 11
+        = (5 + k) ^ 2 - 2 * (5 + k) - 11 := by rw [this]; ring
+    _ = 4 + k * (8 + k) := by ring
+    _ ≥ 4 + 0 := by
+      apply add_le_add_left
+      apply mul_nonneg
+      · exact hk
+      · calc
+          8 + k ≥ 8 := by apply add_le_add_right; exact hk
+          _ ≥ 0 := by numbers
+    _ > 0 := by numbers
